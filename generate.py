e64057be85b9b7be72417519e0f575e00f9b1b90
Dennis Roth <rothdennis92@gmail.com>
2025-11-17 14:19:59 +0100

    fix urls and add tag support

--- generate.py
+++ generate.py
@@ -22,11 +22,11 @@ ID_START=900
 
 IMAGES = {
     'Alma Linux':[
-        {'10': 'https://repo.almalinux.org/almalinux/10/cloud/x86_64/IMAGES/AlmaLinux-10-GenericCloud-latest.x86_64.qcow2'},
-        {'9': 'https://repo.almalinux.org/almalinux/9/cloud/x86_64/IMAGES/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2'},
-        {'8': 'https://repo.almalinux.org/almalinux/8/cloud/x86_64/IMAGES/AlmaLinux-8-GenericCloud-8.10-20240530.x86_64.qcow2'},
+        {'10': 'https://repo.almalinux.org/almalinux/10/cloud/x86_64/images/AlmaLinux-10-GenericCloud-latest.x86_64.qcow2'},
+        {'9': 'https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2'},
+        {'8': 'https://repo.almalinux.org/almalinux/8/cloud/x86_64/images/AlmaLinux-8-GenericCloud-8.10-20240530.x86_64.qcow2'},
     ],
-    'Alpine':[
+    'Alpine Linux':[
         {'3.22': 'https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/cloud/generic_alpine-3.22.2-x86_64-bios-cloudinit-r0.qcow2'},
         {'3.21': 'https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/cloud/generic_alpine-3.21.5-x86_64-bios-cloudinit-r0.qcow2'},
         {'3.20': 'https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/cloud/generic_alpine-3.20.8-x86_64-bios-cloudinit-r0.qcow2'},
@@ -39,12 +39,12 @@ IMAGES = {
         {'Latest': 'https://fastly.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudimg.qcow2'},
     ],
     'CentOS Stream':[
-        {'10': 'https://cloud.centos.org/centos/10-stream/x86_64/IMAGES/CentOS-Stream-GenericCloud-10-latest.x86_64.qcow2'},
-        {'9': 'https://cloud.centos.org/centos/9-stream/x86_64/IMAGES/CentOS-Stream-GenericCloud-x86_64-9-latest.x86_64.qcow2'},
+        {'10': 'https://cloud.centos.org/centos/10-stream/x86_64/images/CentOS-Stream-GenericCloud-10-latest.x86_64.qcow2'},
+        {'9': 'https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-x86_64-9-latest.x86_64.qcow2'},
     ],
     'Debian':[
-        {'13': 'https://cloud.debian.org/IMAGES/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2'},
-        {'12': 'https://cloud.debian.org/IMAGES/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2'},
+        {'13': 'https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2'},
+        {'12': 'https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2'},
     ],
     'Fedora':[
         {'Cloud 43': 'https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2'},
@@ -61,16 +61,16 @@ IMAGES = {
         {'8.10': 'https://yum.oracle.com/templates/OracleLinux/OL8/u10/x86_64/OL8U10_x86_64-kvm-b258.qcow2'},
         {'7.9': 'https://yum.oracle.com/templates/OracleLinux/OL7/u9/x86_64/OL7U9_x86_64-kvm-b257.qcow2'},
     ],
-    'Rocky':[
-        {'10': 'https://dl.rockylinux.org/pub/rocky/10/IMAGES/x86_64/Rocky-10-GenericCloud-Base.latest.x86_64.qcow2'},
-        {'9': 'https://dl.rockylinux.org/pub/rocky/9/IMAGES/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2'},
-        {'8': 'https://dl.rockylinux.org/pub/rocky/8/IMAGES/x86_64/Rocky-8-GenericCloud-Base.latest.x86_64.qcow2'},
+    'Rocky Linux':[
+        {'10': 'https://dl.rockylinux.org/pub/rocky/10/images/x86_64/Rocky-10-GenericCloud-Base.latest.x86_64.qcow2'},
+        {'9': 'https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2'},
+        {'8': 'https://dl.rockylinux.org/pub/rocky/8/images/x86_64/Rocky-8-GenericCloud-Base.latest.x86_64.qcow2'},
     ],
     'Ubuntu':[
-        {'25.10': 'https://cloud-IMAGES.ubuntu.com/releases/25.10/release/ubuntu-25.10-server-cloudimg-amd64.img'},
-        {'25.04': 'https://cloud-IMAGES.ubuntu.com/releases/25.04/release/ubuntu-25.04-server-cloudimg-amd64.img'},
-        {'24.04': 'https://cloud-IMAGES.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img'},
-        {'22.04': 'https://cloud-IMAGES.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img'},   
+        {'25.10': 'https://cloud-images.ubuntu.com/releases/25.10/release/ubuntu-25.10-server-cloudimg-amd64.img'},
+        {'25.04': 'https://cloud-images.ubuntu.com/releases/25.04/release/ubuntu-25.04-server-cloudimg-amd64.img'},
+        {'24.04': 'https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img'},
+        {'22.04': 'https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img'},   
     ],    
     }
 
@@ -200,6 +200,9 @@ subprocess.run(['qm', 'resize', id, 'scsi0', DISK_SIZE])
 
 # enable qemu-guest-agent
 subprocess.run(['qm', 'set', id, '--agent', 'enabled=1,fstrim_cloned_disks=1'])
+
+# add tag
+subprocess.run(['qm', 'set', id, '--tags', os_name.split('-')[0]])
 
 # convert to template
 subprocess.run(['qm', 'template', id])
